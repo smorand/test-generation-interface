bd65089c36fb39327cb7b1ee6762b3e47e3f4dfb
Sébastien MORAND <sebastien.morand@ibm.com>
2026-09-11 11:03:08 +0200

    feat: add vendor/ bundle for offline Windows install (no PyPI access needed)

--- tgi.bat
+++ tgi.bat
@@ -2,10 +2,6 @@
 REM Raccourci de lancement Windows: demarre le serveur TGI sur http://localhost:8080
 REM Double-cliquez sur ce fichier, ou lancez-le depuis PowerShell.
 cd /d "%~dp0"
-set HTTPS_PROXY=https://145.226.163.45:8080
-set HTTP_PROXY=http://145.226.163.45:8080
-set UV_SYSTEM_CERTS=1
-set UV_DEFAULT_INDEX=https://repo.cm-cic.fr/artifactory/api/pypi/all-pypi-dev/simple/
-uv sync
+uv sync --find-links vendor\ --no-index
 uv run tgi
 pause
