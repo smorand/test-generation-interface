456c7826af366402599996f89632581754f46e50
Sébastien MORAND <sebastien.morand@ibm.com>
2026-09-11 10:54:09 +0200

    fix: set HTTP/HTTPS_PROXY in tgi.bat for corporate proxy

--- tgi.bat
+++ tgi.bat
@@ -2,6 +2,8 @@
 REM Raccourci de lancement Windows: demarre le serveur TGI sur http://localhost:8080
 REM Double-cliquez sur ce fichier, ou lancez-le depuis PowerShell.
 cd /d "%~dp0"
+set HTTPS_PROXY=https://145.226.163.45:8080
+set HTTP_PROXY=http://145.226.163.45:8080
 uv sync
 uv run tgi
 pause
