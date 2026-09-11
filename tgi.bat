e7c2029049d274d97eeeaab77029398786b3fa06
Sébastien MORAND <sebastien.morand@ibm.com>
2026-09-11 11:46:55 +0200

    fix: add --no-deps to uv pip install, update requirements.txt with all transitives

--- tgi.bat
+++ tgi.bat
@@ -2,6 +2,6 @@
 REM Raccourci de lancement Windows: demarre le serveur TGI sur http://localhost:8080
 REM Double-cliquez sur ce fichier, ou lancez-le depuis PowerShell.
 cd /d "%~dp0"
-uv pip install --find-links vendor\ --no-index -r vendor\requirements.txt -e .
+uv pip install --find-links vendor\ --no-index --no-deps -r vendor\requirements.txt -e .
 uv run tgi
 pause
