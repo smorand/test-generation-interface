2968bc005b70cfa38600568e14cd1d4b0f3872af
Sébastien MORAND <sebastien.morand@ibm.com>
2026-09-11 11:01:05 +0200

    fix: use CM-CIC Artifactory mirror to bypass PyPI proxy block

--- tgi.bat
+++ tgi.bat
@@ -5,6 +5,7 @@ cd /d "%~dp0"
 set HTTPS_PROXY=https://145.226.163.45:8080
 set HTTP_PROXY=http://145.226.163.45:8080
 set UV_SYSTEM_CERTS=1
+set UV_DEFAULT_INDEX=https://repo.cm-cic.fr/artifactory/api/pypi/all-pypi-dev/simple/
 uv sync
 uv run tgi
 pause
